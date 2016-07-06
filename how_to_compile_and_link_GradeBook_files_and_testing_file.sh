5d91be42b4325926fe20a9a59972befa0f2d1f34
Ben Sandeen <benjaminsandeen2016@u.northwestern.edu>
2016-07-06 00:10:24 -0500

    Removed .swp files

--- how_to_compile_and_link_GradeBook_files_and_testing_file.sh
+++ how_to_compile_and_link_GradeBook_files_and_testing_file.sh
@@ -1,3 +1,4 @@
 g++ -c -o GradeBook.o GradeBook.cpp # compiles but doesn't link GradeBook.cpp
+g++ -c -o Student.o Student.cpp # compiles but doesn't link Student.cpp
 g++ -c -o Testing_GradeBook.o Testing_GradeBook.cpp # compiles but doesn't link Testing_GradeBook.cpp
-g++ -o executable_file_name GradeBook.o Testing_GradeBook.o # finally fully compiles AND LINKS the separate files
+g++ -o executable_file_name GradeBook.o Student.o Testing_GradeBook.o # finally fully compiles AND LINKS the separate files
